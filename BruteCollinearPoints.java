edbcf8f8aa22a0c48460a64b09cb5fb275246858
kalenkovich <e.kalenkovich@gmail.com>
2026-02-06 13:48:24 +0200

    feat: implement BruteCollinearPoints.java

---
+++ BruteCollinearPoints.java
@@ -0,0 +1,87 @@
+import java.util.Arrays;
+
+public class BruteCollinearPoints {
+    private int numberOfSegments;
+    private LineSegment[] lineSegments;
+    private Point[] pointsSorted;
+
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException("points cannot be null");
+        }
+
+        pointsSorted = new Point[points.length];
+        Point point;
+        for (int i = 0; i < points.length; i++) {
+            point = points[i];
+            if (point == null) {
+                throw new IllegalArgumentException("point cannot be null");
+            }
+            pointsSorted[i] = point;
+        }
+        Arrays.sort(pointsSorted);
+
+        Point p, q, r, s;
+        Double pq, pr, ps;
+        lineSegments = new LineSegment[points.length];
+
+        for (int i = 0; i < points.length; i++) {
+            p = pointsSorted[i];
+
+            for (int j = i + 1; j < points.length; j++) {
+                q = pointsSorted[j];
+                pq = p.slopeTo(q);
+
+                for (int k = j + 1; k < points.length; k++) {
+                    r = pointsSorted[k];
+                    pr = p.slopeTo(r);
+
+                    for (int l = k + 1; l < points.length; l++) {
+                        s = pointsSorted[l];
+                        ps = p.slopeTo(s);
+
+                        if (pq == Double.NEGATIVE_INFINITY || pr == Double.NEGATIVE_INFINITY
+                                || ps == Double.NEGATIVE_INFINITY) {
+                            throw new IllegalArgumentException("Duplicate points detected");
+                        }
+
+                        if (pq.equals(pr) && pq.equals(ps)) {
+                            numberOfSegments++;
+                            LineSegment lineSegment = new LineSegment(p, s);
+                            lineSegments[numberOfSegments - 1] = lineSegment;
+
+                        }
+                    }
+                }
+            }
+        }
+
+        lineSegments = Arrays.copyOf(lineSegments, numberOfSegments);
+
+    }
+
+    public int numberOfSegments() {
+        return numberOfSegments;
+    }
+
+    public LineSegment[] segments() {
+        return lineSegments;
+    }
+
+    public static void main(String[] args) {
+        Point[] points = new Point[16];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                points[i * 4 + j] = new Point(i, j);
+            }
+        }
+
+        BruteCollinearPoints bcp = new BruteCollinearPoints(points);
+        System.out.println("Number of segments: " + bcp.numberOfSegments());
+        for (LineSegment segment : bcp.segments()) {
+            System.out.println(segment);
+        }
+
+    }
+
+}
