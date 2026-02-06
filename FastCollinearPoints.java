d7df6f5c36e1ab4ed3894ffed34f89e7b1151248
kalenkovich <e.kalenkovich@gmail.com>
2026-02-06 18:14:54 +0200

    fix (3/n): drop duplicate segments
    - The slope doesn't depend on the order of the two points. Because of that, every point out for 4 of every segment will be point p once creating 3 duplicates for each segment. We now remove duplicates at the end.
    - The segments ends were incorrect as well because point p and s were used while the order of p, q, r, s was arbitrary (which is related to the previous point: if we used angle, instead of slope, p would be guaranteed to be at one of the ends). Switching to min(p, q, r, s) and max(p, q, r, s) fixed that.

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -10,31 +10,95 @@ public class FastCollinearPoints {
         }
 
         Point[] aux = new Point[points.length];
-        for (int i = 0; i < points.length; i++) {
-            if (points[i] == null) {
-                throw new IllegalArgumentException("point cannot be null");
-            }
-            aux[i] = points[i];
-        }
-
         double[] slopes = new double[points.length];
-        lineSegments = new LineSegment[points.length];
+        int n = points.length;
+        lineSegments = new LineSegment[n * (n - 1) * (n - 2) * (n - 3) / 24];
 
         for (int i = 0; i < points.length; i++) {
-            Arrays.sort(aux, aux[i].slopeOrder());
+            for (int j = 0; j < points.length; j++) {
+                if (i == j) {
+                    aux[i] = points[0];
+                    aux[0] = points[i];
+                }
+                else {
+                    aux[j] = points[j];
+                }
+            }
+
+            Arrays.sort(aux, aux[0].slopeOrder());
             for (int j = 1; j < points.length; j++) {
                 slopes[j] = aux[0].slopeTo(aux[j]);
             }
             for (int j = 1; j < points.length - 2; j++) {
                 if (slopes[j] == slopes[j + 1] && slopes[j] == slopes[j + 2]) {
                     numberOfSegments++;
-                    lineSegments[numberOfSegments - 1] = new LineSegment(aux[0], aux[j + 2]);
+                    lineSegments[numberOfSegments - 1] = new LineSegment(
+                            min(aux[0], aux[j], aux[j + 1], aux[j + 2]),
+                            max(aux[0], aux[j], aux[j + 1], aux[j + 2]));
                 }
             }
         }
 
+        // remove duplicates
+        Arrays.sort(lineSegments, (ls1, ls2) -> {
+            if (ls1 == null && ls2 == null) {
+                return 0;
+            }
+            if (ls1 == null) {
+                return 1;
+            }
+            if (ls2 == null) {
+                return -1;
+            }
+            return ls1.toString().compareTo(ls2.toString());
+        });
+
+        int i = 1;
+        int originalNumberOfSegments = numberOfSegments;
+        for (int j = 1; j < originalNumberOfSegments; j++) {
+            if (lineSegments[j].toString().equals(
+                    lineSegments[j - 1].toString())) {
+                numberOfSegments--;
+            }
+            else {
+                lineSegments[i] = lineSegments[j];
+                i++;
+            }
+        }
+
+        // drop nulls and duplicates
         lineSegments = Arrays.copyOf(lineSegments, numberOfSegments);
-    }     // finds all line segments containing 4 or more points
+
+
+    }  // finds all line segments containing 4 or more points
+
+    private static Point min(Point p1, Point p2, Point p3, Point p4) {
+        Point min = p1;
+        if (p2.compareTo(min) < 0) {
+            min = p2;
+        }
+        if (p3.compareTo(min) < 0) {
+            min = p3;
+        }
+        if (p4.compareTo(min) < 0) {
+            min = p4;
+        }
+        return min;
+    }
+
+    private static Point max(Point p1, Point p2, Point p3, Point p4) {
+        Point max = p1;
+        if (p2.compareTo(max) > 0) {
+            max = p2;
+        }
+        if (p3.compareTo(max) > 0) {
+            max = p3;
+        }
+        if (p4.compareTo(max) > 0) {
+            max = p4;
+        }
+        return max;
+    }
 
 
     public int numberOfSegments() {
